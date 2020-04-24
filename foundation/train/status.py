d7260a02fc59941351ac62e8b92960fa670588d1
Felix <felix.leeb@hotmail.com>
2020-04-24 20:46:31 +0200

    update

--- foundation/train/status.py
+++ foundation/train/status.py
@@ -4,28 +4,130 @@ import subprocess
 from tabulate import tabulate
 import numpy as np
 
+from .. import util
+
 from .registry import Script
 
-@Script('cstat', use_config=True)
-def get_status(A):
+colattrs = ['ClusterId', 'ProcId', 'JobStatus', 'Args', 'RemoteHost']
+
+_status_codes = {
+	'0': 'Unexpanded',
+	'1': 'Idle',
+	'2': 'Running',
+	'3': 'Removed',
+	'4': 'Completed',
+	'5': 'Held',
+	'6': 'Submission_err',
+
+}
+
+def parse_jobexec(raw, info=None):
+	*root, jdir, jexe = raw.split('/')
+	
+	num, date = jdir.split('_')
+	num = int(num[3:])
+	
+	if info is None:
+		info = util.tdict()
+	
+	info.num = num
+	info.date = date
+	info.exe = jexe
+	info.path = os.path.dirname(raw)
+	
+	return info
+
+def parse_remotehost(raw):
+	s, g = raw.split('.')[0].split('@')
+	s = s.split('_')[-1]
+	return f'{s}{g}'
+
+def parse_job_status(info):
+	if 'ClusterId' in info:
+		info.job_id = info.ClusterId
+		del info.ClusterId
+		
+	if 'ProcId' in info:
+		info.proc_id = info.ProcId
+		del info.ProcId
+		
+	if 'JobStatus' in info:
+		info.status = _status_codes[info.JobStatus]
+		del info.JobStatus
+		
+	if 'Args' in info:
+		parse_jobexec(info.Args, info)
+		del info.Args
+	
+	if 'RemoteHost' in info:
+		info.host = parse_remotehost(info.RemoteHost)
+		del info.RemoteHost
+	
+	return info
+
+def collect_q_cmd():
+	print('Getting job status...', end='')
+	
+	# raw = subprocess.check_output(['condor_q', 'fleeb', '-af', 'ClusterId', 'ProcId', 'Args', 'JobStatus', 'RemoteHost', 'Env'])
+	raw = subprocess.check_output(['condor_q', 'fleeb', '-af:t'] + colattrs).decode()
+	
+	if len(raw) == 0:
+		print(' No jobs running.')
+	else:
+		print(' found {} jobs'.format(len(raw)))
+	
+	R = util.MultiDict(parse_job_status(util.tdict(zip(colattrs, line.split('\t')))) for line in raw.split('\n'))
+	
+	return R
+
+def peek_file(opath, peek=0):
+	if os.path.isfile(opath):
+		with open(opath, 'r') as f:
+			return f.readlines()[-peek:]
+	return None
+
+def print_current(full, simple=True):
+	table = []
+	for info in full:
+		table.append([f'{info.job_id}.{info.proc_id}', f'{info.num}-{info.proc_id}', f'{info.date}', f'{info.status}'])
+		
+	if simple:
+		print(tabulate(table, ['ClusterId', 'JobId', 'StartDate', 'Status']))
+	
+	else:
+		for row, info in zip(table, full):
+			head = '----- {} -----'.format(' - '.join(row))
+			tail = '-'*len(head)
+			
+			print(head)
+			print(info.peek)
+			print(tail)
+			print()
+
+			
+@Script('cstat')
+def get_status(peek=None):
 	'''
 	Script to get a status of the cluster jobs
 	'''
 	
 	assert 'FOUNDATION_RUN_MODE' in os.environ and os.environ['FOUNDATION_RUN_MODE'] == 'cluster', 'Should be the cluster'
 	
-	print(subprocess.check_output(['ls', '-l']).decode())
-	
-	print('Getting job status...', end='')
-	raw = subprocess.check_output(['condor_q', 'fleeb', '-af', 'jl,'])
-	print(' received {} bytes'.format(len(raw)))
-	
 	print()
 	
-	if len(raw) == 0:
+	current = collect_q_cmd()
+	
+	if len(current) == 0:
 		print('No jobs running.')
+		return 0
 	
+	if peek is not None:
+		for info in current:
+			if 'path' in info and 'num' in info:
+				opath = os.path.join(info.path, info.num)
+				info.peek = peek_file(opath, peek)
 	
+	print_current(current, simple=peek is None)
 	
 	return 0
 
--- foundation/util/misc.py
+++ foundation/util/misc.py
@@ -1,5 +1,6 @@
 
 import sys, os
+import traceback
 import torch
 import numpy as np
 #from itertools import imap
@@ -11,6 +12,8 @@ import torch.nn as nn
 import torch.multiprocessing as mp
 # from torch.utils.data.dataloader import ExceptionWrapper
 import random
+
+from .farming import make_ghost
 
 FD_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -50,6 +53,68 @@ class NS(tdict): # NOTE: avoid hasattr! - always returns true (creating new attr
 
 	def __repr__(self):
 		return '{}{}{}'.format('{{', ', '.join(['{}:{}'.format(repr(k), repr(v)) for k,v in self.items()]), '}}')
+
+class MultiDict(tlist):
+	'''
+	All elements should be dicts (or ideally tdicts)
+	'''
+	
+	def __init__(self, *args, _type=tdict, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.__dict__['_el_type'] = _type
+	
+	def by(self, key, skip=True):
+		for x in self:
+			if key in x:
+				yield x[key]
+			elif not skip:
+				yield None
+	
+	def by_items(self, key, skip=True):
+		for x in self:
+			if key in x:
+				yield x[key], x
+			elif not skip:
+				yield None, None
+
+	def new(self, *args, **kwargs):
+		self.append(self._el_type(*args, **kwargs))
+
+	def map(self, fn, indexed=False, safe=False, pbar=None, reduce=None):
+		'''
+		fn is a callable taking one run as input
+		'''
+
+		outs = []
+
+		seq = self if pbar is None else pbar(self)
+
+		for i, x in enumerate(seq):
+			try:
+				inp = (i,x) if indexed else (x,)
+				out = fn(*inp)
+				outs.append(out)
+			except Exception as e:
+				if safe:
+					print(f'elm {i} failed')
+					traceback.print_exc()
+				else:
+					raise e
+
+		if pbar is not None:
+			seq.close()
+
+		if reduce is not None:
+			return reduce(outs)
+		return outs
+
+	def through(self, **map_kwargs):
+
+		def _execute(fn, args=[], kwargs={}):
+			return self.map(lambda run: fn(run, *args, **kwargs),
+			                **map_kwargs)
+
+		return make_ghost(self._el_type, _execute)
 
 
 def deep_get(tree, keys):
