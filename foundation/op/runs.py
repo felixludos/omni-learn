c840d05eed367dbb08b51b4c2b153a06b143efdd
Felix Leeb <felix.leeb@tuebingen.mpg.de>
2020-09-28 12:52:29 +0200

    cleaned up cluster runs

--- foundation/op/runs.py
+++ foundation/op/runs.py
@@ -136,7 +136,7 @@ class Checkpoint_Flag(Exception):
 class NoOverwriteError(Exception):
 	pass
 
-@fig.Script('load_run', description='Load a new or existing run') # the script just loads a run
+@fig.Script('load-run', description='Load a new or existing run') # the script just loads a run
 def load_run(A):
 	try:
 		A = A.sub('run')
@@ -1043,43 +1043,6 @@ class Run:
 	
 	# endregion
 
-@fig.AutoModifier('cls-run')
-class OnCluster(Run):
-	
-	def startup(self):
-		
-		super().startup()
-		
-		save_dir = self.save_dir
-		
-		if save_dir is not None and 'JOBDIR' in os.environ:
-			jobdir = os.environ['JOBDIR']
-			cname = 'checkpoints{}.txt'.format(os.environ['PROCESS_ID'])
-			
-			if cname not in os.listdir(jobdir):
-				
-				# register job
-				if 'JOB_REGISTRY_PATH' in os.environ:
-					rpath = os.environ['JOB_REGISTRY_PATH']
-					reg = load_yaml(rpath) if os.path.isfile(rpath) else []
-					reg.append({
-						'timestamp': get_now(),
-						
-						'id': os.environ['JOB_ID'].split('#')[-1],
-						'num': int(os.environ['JOB_NUM']),
-						'proc': int(os.environ['PROCESS_ID']),
-						
-						'host': socket.gethostname(),
-						
-						'run': save_dir,
-						'job': jobdir,
-					})
-					save_yaml(reg, rpath)
-					
-				with open(os.path.join(jobdir, cname), 'w') as f:
-					f.write(os.path.basename(save_dir))
-				print('[Saved checkpoint dir for restarts]')
-		
 		
 @fig.AutoModifier('timed-run')
 class Timed(Run):
