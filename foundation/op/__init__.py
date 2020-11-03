54b62cd33cef8394108cbb90ca10ddeb7044c5ab
flxleeb <flxleeb@amazon.com>
2020-11-03 10:05:44 +0100

    get dataset

--- foundation/op/__init__.py
+++ foundation/op/__init__.py
@@ -2,7 +2,7 @@
 from .datasets import MNIST
 
 from .model import load_model
-from .data import load_data
+from .data import load_data, Dataset
 from .evaluation import eval_model
 from .training import iterative_training
 from .report import get_report
