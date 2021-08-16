cd5e4fbb934596fb27d7ae1a96b8bdbe6d92f030
felixludos <felix.leeb@hotmail.com>
2021-08-16 14:28:52 +0200

    elbo + bpd

--- omnilearn/eval/__init__.py
+++ omnilearn/eval/__init__.py
@@ -1,3 +1,4 @@
 
 from . import fid
 from .evaluator import Evaluator
+from .stats import elbo, bits_per_dim
---
+++ omnilearn/eval/stats.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+from torch.nn import functional as F
+
+from ..util import Distribution
+
+
+def negative_log_likelihood(original, reconstruction, batch_mean=True):
+	if isinstance(reconstruction, Distribution):
+		return -reconstruction.log_prob(original).view(original.size(0),-1).sum(-1).mean()
+	if len(reconstruction.shape) > 2:
+		return F.binary_cross_entropy(reconstruction, original, reduction='none')\
+			.view(original.size(0),-1).sum(-1).mean()
+	return F.mse_loss(reconstruction, original).view(original.size(0),-1).sum(-1).mean()
+
+
+def elbo(original, reconstruction, kl, batch_mean=True):
+	nll = negative_log_likelihood(original, reconstruction, batch_mean=batch_mean)
+	return nll - kl
+
+
+def bits_per_dim(original, reconstruction, batch_mean=True):
+	return negative_log_likelihood(original, reconstruction, batch_mean=batch_mean) \
+	       / np.log(2) / int(np.prod(original.shape))
+
+
+
